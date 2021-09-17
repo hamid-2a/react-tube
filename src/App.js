0a44bc69484e269707c1a7a55854d4ab96e2bb61
hamid-2a <hamidasghari2829@gmail.com>
2021-09-17 20:04:08 +0430

    The environment was set. antd installed. Ready to go!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { Card, Avatar } from 'antd';
+import 'antd/dist/antd.css';
+const { Meta } = Card;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card
+        hoverable
+        style={{ width: 240 }}
+        cover={<img alt="example" src="https://images.unsplash.com/photo-1470202456367-bd76a65211d2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" />}
+      >
+        <Meta
+          avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+          title="This is video title"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit." />
+      </Card>
+
+
     </div>
   );
 }
+
 
 export default App;
